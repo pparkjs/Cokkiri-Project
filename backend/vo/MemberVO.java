9badf7ec96c614df46ba2b0e466263d03b858e6c
pparkjs <qkrwjdtn741@naver.com>
2023-04-08 09:48:41 +0900

    member 테이블 VO 추가

---
+++ backend/vo/MemberVO.java
@@ -0,0 +1,84 @@
+package vo;
+
+public class MemberVO {
+	private String mem_id;
+	private String mem_pw;
+	private String mem_name;
+	private String mem_tel;
+	private String mem_mail;
+	private String mem_add;
+	private String mem_bir;
+	private String mem_gend;
+	private String mem_nickname;
+	private String mem_image;
+	private String admin_auth;
+	
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getMem_pw() {
+		return mem_pw;
+	}
+	public void setMem_pw(String mem_pw) {
+		this.mem_pw = mem_pw;
+	}
+	public String getMem_name() {
+		return mem_name;
+	}
+	public void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+	public String getMem_tel() {
+		return mem_tel;
+	}
+	public void setMem_tel(String mem_tel) {
+		this.mem_tel = mem_tel;
+	}
+	public String getMem_mail() {
+		return mem_mail;
+	}
+	public void setMem_mail(String mem_mail) {
+		this.mem_mail = mem_mail;
+	}
+	public String getMem_add() {
+		return mem_add;
+	}
+	public void setMem_add(String mem_add) {
+		this.mem_add = mem_add;
+	}
+	public String getMem_bir() {
+		return mem_bir;
+	}
+	public void setMem_bir(String mem_bir) {
+		this.mem_bir = mem_bir;
+	}
+	public String getMem_gend() {
+		return mem_gend;
+	}
+	public void setMem_gend(String mem_gend) {
+		this.mem_gend = mem_gend;
+	}
+	public String getMem_nickname() {
+		return mem_nickname;
+	}
+	public void setMem_nickname(String mem_nickname) {
+		this.mem_nickname = mem_nickname;
+	}
+	public String getMem_image() {
+		return mem_image;
+	}
+	public void setMem_image(String mem_image) {
+		this.mem_image = mem_image;
+	}
+	public String getAdmin_auth() {
+		return admin_auth;
+	}
+	public void setAdmin_auth(String admin_auth) {
+		this.admin_auth = admin_auth;
+	}
+	
+	
+}
