392938d8fe5239b0c43e9efa3d25a8956042adc5
junghappy <dmadma7917@naver.com>
2023-04-13 13:59:24 +0900

    test : 아이디 비밀번호 중복체크

--- backend/service/memberService/IMemberService.java
+++ backend/service/memberService/IMemberService.java
@@ -16,20 +16,20 @@ public interface IMemberService {
 	public int memberInsert(MemberVO memVo);
 	
 	// 전화번호를 입력받고, 생성한 난수 반환
-	public String sendRandomMessage(String mem_tel);
+	public String sendRandomMessage(String memTel);
 	
 	// 회원 전화번호 중복 체크
-	public int memberTelCount(String mem_tel);
+	public int memberTelCount(String memTel);
 	
 	// 회원 아이디 중복체크
-	public int incheck(String mem_id);
+	public int incheck(String memId);
 	
 	// 회원 닉네임 중복체크
-	public int nicknamecheck(String mem_nickname);
+	public int nicknamecheck(String memNickname);
 
 	public int selectIsMember(String id);
 	
-	public MemberVO selectMemberinfo(String mem_id);
+	public MemberVO selectMemberinfo(String memId);
 	
 	public MemberVO selectMemberinfoByNick(String nickname);
 
