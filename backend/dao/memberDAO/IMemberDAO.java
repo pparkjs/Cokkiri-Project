3eaca30527a83755ca1ee3e2de863e44d8e41c86
junghappy <dmadma7917@naver.com>
2023-04-13 10:00:16 +0900

    feat: Phone number, ID, nickname duplicate check, sign up

--- backend/dao/memberDAO/IMemberDAO.java
+++ backend/dao/memberDAO/IMemberDAO.java
@@ -6,5 +6,16 @@ import vo.MemberVO;
 
 public interface IMemberDAO {
 	//회원의 정보를 가져오는 메소드
-		public List<MemberVO> memberAllList();
+	public List<MemberVO> memberAllList();
+		
+	public int memberInsert(MemberVO memVo);
+	
+	// 회원 전화번호 중복 체크
+	public int memberTelCount(String mem_tel);
+	
+	// 회원 아이디 중복체크
+	public int incheck(String mem_id);
+	
+	// 회원 닉네임 중복체크
+	public int nicknamecheck(String mem_nickname);
 }
