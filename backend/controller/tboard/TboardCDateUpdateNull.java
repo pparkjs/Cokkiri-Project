e7aca8410bf5ce1db6df1e413af36009c985275f
junghappy <dmadma7917@naver.com>
2023-04-18 15:45:59 +0900

    feat : Cancel transaction completion
    거래 완료를 취소할 때 거래완료 일자를 초기상태(NULL)로 변경하고 다시 판매상태(팝니다, 삽니다, 나눔)을 받아오는 기능 추가

---
+++ backend/controller/tboard/TboardCDateUpdateNull.java
@@ -0,0 +1,48 @@
+package controller.tboard;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import service.tboardService.TboardServiceImpl;
+
+@WebServlet("/tboardCDateUpdateNull.do")
+public class TboardCDateUpdateNull extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		int tboardId = Integer.parseInt(request.getParameter("tboard_id"));
+		
+		TboardServiceImpl service = TboardServiceImpl.getInstance();
+		int res = service.updateTboardCompleteDateNull(tboardId);
+		String tboardState = service.selectTboardState(tboardId);
+		
+		
+		JsonObject jsonObject = new JsonObject();	
+		if(res == 1) {
+			jsonObject.addProperty("result", "ok");
+			jsonObject.addProperty("tboardState", tboardState);
+		} else {
+			jsonObject.addProperty("result", "no");
+		}
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(jsonObject);
+		response.getWriter().write(json);
+		response.flushBuffer();
+	}
+
+}
