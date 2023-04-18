5db55bf20fb594bf2d3a98062336bb353ed4bf2f
junghappy <dmadma7917@naver.com>
2023-04-18 16:02:28 +0900

    feat : Create MypageTboardVO used in My Page
    마이페이지에서 사용되는 MypageTboardVO alias 추가

---
+++ backend/vo/MypageTboardVO.java
@@ -0,0 +1,92 @@
+package vo;
+
+public class MypageTboardVO {
+	private String timg_save_name;
+	private String tboard_title;
+	private String mem_add;
+	private int tboard_imgNo;
+	private String wishcnt;
+	private String tboard_id;
+	private String mem_id;
+	private String tboard_completedate;
+	private String tboard_state;
+	private int tboard_price;
+	private int tboard_hit;
+	private int mylistcnt;
+	
+	
+	public String getTimg_save_name() {
+		return timg_save_name;
+	}
+	public void setTimg_save_name(String timg_save_name) {
+		this.timg_save_name = timg_save_name;
+	}
+	public String getTboard_title() {
+		return tboard_title;
+	}
+	public void setTboard_title(String tboard_title) {
+		this.tboard_title = tboard_title;
+	}
+	public String getMem_add() {
+		return mem_add;
+	}
+	public void setMem_add(String mem_add) {
+		this.mem_add = mem_add;
+	}
+	public int getTboard_imgNo() {
+		return tboard_imgNo;
+	}
+	public void setTboard_imgNo(int tboard_imgNo) {
+		this.tboard_imgNo = tboard_imgNo;
+	}
+	public String getWishcnt() {
+		return wishcnt;
+	}
+	public void setWishcnt(String wishcnt) {
+		this.wishcnt = wishcnt;
+	}
+	public String getTboard_id() {
+		return tboard_id;
+	}
+	public void setTboard_id(String tboard_id) {
+		this.tboard_id = tboard_id;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public int getTboard_price() {
+		return tboard_price;
+	}
+	public void setTboard_price(int tboard_price) {
+		this.tboard_price = tboard_price;
+	}
+	public int getTboard_hit() {
+		return tboard_hit;
+	}
+	public void setTboard_hit(int tboard_hit) {
+		this.tboard_hit = tboard_hit;
+	}
+	public int getMylistcnt() {
+		return mylistcnt;
+	}
+	public void setMylistcnt(int mylistcnt) {
+		this.mylistcnt = mylistcnt;
+	}
+	public String getTboard_completedate() {
+		return tboard_completedate;
+	}
+	public void setTboard_completedate(String tboard_completedate) {
+		this.tboard_completedate = tboard_completedate;
+	}
+	public String getTboard_state() {
+		return tboard_state;
+	}
+	public void setTboard_state(String tboard_state) {
+		this.tboard_state = tboard_state;
+	}
+	
+	
+}
