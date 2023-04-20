5ab141b3e745966518024cc9dcdec887aa9d645b
sunghee95 <sunghee9647@gmail.com>
2023-04-20 10:45:34 +0900

    feat: 지도에 관한 기능 추가

--- frontend/js/pboard.js
+++ frontend/js/pboard.js
@@ -2,6 +2,9 @@
  * 
  */
 
+
+
+
 //흘러간 시간대별 날짜 계산하기
  function elapsedTime(date) {
         const start = new Date(date);
@@ -194,6 +197,30 @@ function notifyCheckView(mr){
 }
 
 
+
+// 맵 위치 저장 
+function saveLocation(){
+	$.ajax({
+		url : `${mypath}/PboardWrite.do`,
+		type : 'get',
+		data : {"title" : title,
+				"content" : content,
+				"place" : place				
+		},
+		
+		success : function(res){
+			alert(res)
+			
+			
+		},
+		
+		error : function(xhr){
+			alert("상태 : " + xhr.status);
+		},
+		dataType : 'json'
+		
+	})
+}
 
 
 //게시글 리스트 가져오기
